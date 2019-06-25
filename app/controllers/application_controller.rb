438b817ff2fa5c42ce612fd7e7b0717241983b5d
BusterStatus <matthew.serre@gmail.com>
2019-06-25 14:19:04 -0700

    Create users_controller file with basic routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,15 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
+  helpers do
+    def self.current_user(session_hash)
+      @user = User.find_by(id: session_hash[:user_id])
+      @user
+    end
+    
+    def self.is_logged_in?(session_hash)
+      !!self.current_user(session_hash)
+    end
   end
 
 end
